078fb08109452c8b005b33fbf321a76bc5be981a
Ottman <otmanbboy@gmail.com>
2023-06-16 11:06:04 +0100

    variable if else

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
+ * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-
-	int n;
-	int m;
-	int l;
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
-		for (l = 50; l < 58; l++)
+		for (m = 49; m < 58; m++)
 		{
-			if (l > m && m > n)
+			for (l = 50; l < 58; l++)
 			{
-				putchar(n);
-				putchar(m);
-				putchar(l);
-				if (n != 55 || m != 56)
+				if (l > m && m > n)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
