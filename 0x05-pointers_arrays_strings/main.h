b849bedc4dd11af41cce716a0daa20891323559d
Ottman <otmanbboy@gmail.com>
2023-06-27 01:54:03 +0100

    pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
+int _putchar(char c);
