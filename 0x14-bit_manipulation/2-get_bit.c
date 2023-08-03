c1980598413a6663b0ca335b96da372c5a948180
Ottman <otmanbboy@gmail.com>
2023-08-03 22:45:36 +0100

    014

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - gets the value of a bit at a given index
- * @n: the number 
- * @index: index staring from 0 of the bit required
- * Return: the converted value
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int bit_val;
+
 	if (index > 63)
 		return (-1);
-	return ((n >> index) & 1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
