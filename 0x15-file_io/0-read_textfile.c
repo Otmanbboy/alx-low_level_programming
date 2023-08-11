3a22a8f4db4b8b0b0156d64b37083d05d0e8009d
Ottman <otmanbboy@gmail.com>
2023-08-11 14:21:28 +0100

    013

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,42 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
- */
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: Input Name File
+ * @letters: Input Number
+ *
+ * Return: Depend Condition
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int f_op, f_rd, f_wr;
+	char *buffer;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (filename == NULL)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	close(fd);
-	return (w);
+	f_op = open(filename, O_RDONLY);
+	if (f_op < 0)
+		return (0);
+	buffer = (char *) malloc(letters * sizeof(char));
+	if (buffer == NULL)
+		return (0);
+	f_rd = read(f_op, buffer, letters);
+	if (f_rd < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[f_rd] = '\0';
+	f_wr = write(STDOUT_FILENO, buffer, f_rd);
+	if (f_wr < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(f_op);
+	return (f_wr);
 }
